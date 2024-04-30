8f2f68c0da37493e126cacbc333706c7c0a7c055
unknown <salim16laghrib@gmail.com>
2024-04-30 19:32:42 +0100

    page welcome de applcation avand de login

---
+++ mobile_app/lib/widget/Text_widget.dart
@@ -0,0 +1,32 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+//hada fih le wiget dayl le text 
+Widget text24({String name = '', Color color = Colors.black}) {
+  return Text(
+    name,
+    style: TextStyle(color: color, fontSize: 24, fontWeight: FontWeight.w900),
+  );
+}
+
+Widget text18({String name = '', Color color = Colors.black}) {
+  return Text(
+    name,
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      color: color,
+      fontSize: 18,
+    ),
+  );
+}
+Widget text36({String name = '', Color color = Colors.white}) {
+  return Text(
+    name,
+    textAlign: TextAlign.center,
+    style: TextStyle(
+      color: color,
+      fontSize: 36,
+    ),
+  );
+}
+
